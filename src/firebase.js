be8fc0696fea4e0e9439b7ed5110f799a8a8194f
Codymwon <aadithmathew2017@gmail.com>
2025-11-29 16:33:12 +0530

    security update

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCQXpDzjHWv-vx5cFCHVyfDC6DFmUjownA",
-    authDomain: "swachify-635d8.firebaseapp.com",
-    projectId: "swachify-635d8",
-    storageBucket: "swachify-635d8.firebasestorage.app",
-    messagingSenderId: "812219162510",
-    appId: "1:812219162510:web:7db2c67a6d25cced708edf"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
